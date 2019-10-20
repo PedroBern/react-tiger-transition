8487e4fe51be7e4d315e03f39dd81a83b7820bad
Pedro Bern <pedrobermoreira@gmail.com>
2019-10-19 22:09:58 -0300

    move jsx to js on boolcsstransition

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
     module : {
         rules : [
             {
-              test : /\.(jsx?)$/,
+              test : /\.(js)$/,
               loader:'babel-loader',
               include: src,
             },
