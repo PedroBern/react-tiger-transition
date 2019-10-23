57df39fd361be896b314c240e11a73bdf8ccda91
Pedro Bern <pedrobermoreira@gmail.com>
2019-10-23 15:38:11 -0200

    more utils test

--- test/utils/ReplaceBackground.test.js
+++ test/utils/ReplaceBackground.test.js
@@ -1,19 +1,34 @@
 import  { ReplaceBackground } from '../../src/utils';
 
-test('Replace body background color', () => {
-  const newBackground = new ReplaceBackground('black')
-  expect(document.body.style.backgroundColor).toBe('');
-  newBackground.fake();
-  expect(document.body.style.backgroundColor).toBe('black');
-  newBackground.original();
-  expect(document.body.style.backgroundColor).toBe('');
-});
 
-test('Replace body background color from colored body', () => {
-  document.body.style.backgroundColor = 'white';
-  const newBackground = new ReplaceBackground('black')
-  newBackground.fake();
-  expect(document.body.style.backgroundColor).toBe('black');
-  newBackground.original();
-  expect(document.body.style.backgroundColor).toBe('white');
+describe('ReplaceBackground', () => {
+
+  test('replace body background color', () => {
+    const newBackground = new ReplaceBackground('black')
+    expect(document.body.style.backgroundColor).toBe('');
+    newBackground.fake();
+    expect(document.body.style.backgroundColor).toBe('black');
+    newBackground.original();
+    expect(document.body.style.backgroundColor).toBe('');
+  });
+
+  test('replace body background color from colored body', () => {
+    document.body.style.backgroundColor = 'white';
+    const newBackground = new ReplaceBackground('black')
+    newBackground.fake();
+    expect(document.body.style.backgroundColor).toBe('black');
+    newBackground.original();
+    expect(document.body.style.backgroundColor).toBe('white');
+  });
+
+  test('dont replace if the background color is the same', () => {
+    document.body.style.backgroundColor = 'white';
+    const newBackground = new ReplaceBackground()
+    newBackground.fake();
+    expect(document.body.style.backgroundColor).toBe('rgb(51, 51, 51)');
+    document.body.style.backgroundColor = 'white';
+    newBackground.original();
+    expect(document.body.style.backgroundColor).toBe('white');
+  });
+
 });
