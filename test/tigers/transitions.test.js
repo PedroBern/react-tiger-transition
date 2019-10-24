4af684ec7cdfcfe2bd7c99211937f7f39879e598
Pedro Bern <pedrobermoreira@gmail.com>
2019-10-23 23:19:48 -0200

    more transitions tests

--- test/tigers/transitions.test.js
+++ test/tigers/transitions.test.js
@@ -8,13 +8,27 @@ describe('transition', () => {
       describe(key, () => {
 
         test('is function', () => {
-          expect(typeof transitions[key]).toBe('function')
+          expect(typeof transitions[key]).toBe('function');
         });
 
         test('return object', () => {
           expect(Object.prototype.toString.call({...transitions[key]()}))
-          .toBe('[object Object]')
+          .toBe('[object Object]');
         });
+
+        if (key.indexOf('InRules') > -1) {
+          test('return buildTransitionIn', () => {
+            expect(transitions[key]().toString())
+            .toStrictEqual(transitions.buildTransitionIn().toString());
+          });
+        }
+
+        else if (key.indexOf('OutRules') > -1) {
+          test('return buildTransitionOut', () => {
+            expect(transitions[key]().toString())
+            .toStrictEqual(transitions.buildTransitionOut().toString());
+          });
+        }
 
       });
 
