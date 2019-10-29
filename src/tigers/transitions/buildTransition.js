7bda5517427ea1b4e7dc379ba8453e50e344f3a2
Pedro Bern <pedrobermoreira@gmail.com>
2019-10-29 18:04:21 -0200

    workaround for isAppering with animejs

--- src/tigers/transitions/buildTransition.js
+++ src/tigers/transitions/buildTransition.js
@@ -1,15 +1,34 @@
+import { addClass, removeClass } from '../../utils';
+
 export const buildTransitionIn = ({
   transition = () => {},
   replaceBackground = null,
 } = {}) => ({
-  onEnter: () => {
+  onEnter: (node, isAppearing) => {
     if (replaceBackground) replaceBackground.fake();
+    if (isAppearing) addClass(node, 'isAppearing');
   },
-  onEntering: (node) => {
-    transition(node);
+  onEntering: (node, isAppearing) => {
+    if (isAppearing) {
+      /**
+       * v2 animates with animejs, that animates directyly on node,
+       * on 'isAppearing' it gets a glitch on animatio,
+       * as a workaround I add 100ms to begin the animation, it solves
+       * all cases for now.
+       * Also we need to set the class 'isAppearing' on 'onEnter' and
+       * remove on 'onEntered', this class is just an opacity=0,
+       * preventing the node to be visible before the animation starts
+       */
+      setTimeout(() => transition(node),
+        100);
+    }
+    else {
+      transition(node);
+    }
   },
-  onEntered: () => {
+  onEntered: (node, isAppearing) => {
     if (replaceBackground) replaceBackground.original();
+    if (isAppearing) removeClass(node, 'isAppearing');
   },
 });
 
