7d27c95a917127094dfa78ecd48f8bd403756d4a
Pedro Bern <pedrobermoreira@gmail.com>
2019-10-31 16:01:52 -0200

    fix travis test babel config

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,26 @@
-module.exports = {
-   "plugins": [
-     "@babel/plugin-proposal-class-properties",
-     ["babel-plugin-transform-react-remove-prop-types", {
-      "removeImport": true,
-      "ignoreFilenames": ["node_modules"]
-    }]
-   ],
-   "presets": [
-     "@babel/preset-react",
-     ["@babel/preset-env", {loose: true}]
-   ]
-};
+if (process.env.NODE_ENV === "test") {
+  module.exports = {
+    "plugins": [
+        "@babel/plugin-transform-runtime",
+        "@babel/plugin-proposal-class-properties",
+      ],
+      "presets": [
+        "@babel/preset-react",
+        ["@babel/preset-env", {"useBuiltIns": false}]
+      ]
+  };
+} else {
+  module.exports = {
+     "plugins": [
+       "@babel/plugin-proposal-class-properties",
+       ["babel-plugin-transform-react-remove-prop-types", {
+        "removeImport": true,
+        "ignoreFilenames": ["node_modules"]
+      }]
+     ],
+     "presets": [
+       "@babel/preset-react",
+       ["@babel/preset-env", {loose: true}]
+     ]
+  };
+}
