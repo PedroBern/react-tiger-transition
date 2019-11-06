7520930cd34eae61e670b42b9ed3f97b11a76c73
Pedro Bern <pedrobermoreira@gmail.com>
2019-11-06 19:05:55 -0200

    better link docs

--- src/Link.js
+++ src/Link.js
@@ -9,33 +9,11 @@ import { withRouterAndRef } from './utils';
 /**
  * @description
  * Change transitions on the fly. The next transition is evaluated before
- * the route change, allowing to go or arrive to with different transitions
- * for the same routes.
+ * the route change, allowing to easily reach and leave the same url with
+ * different transitions.
  *
  * @afterProps
  * \*Ref and other props are passed to [react router `<Link />`](https://reacttraining.com/react-router/web/api/Link).
- *
- * @example
- * import { Link, glide } from "react-tiger-transition";
- *
- * // default transition
- * <Link to='/a' transition={glide}>
- *   A
- * </Link>
- *
- * // default transition with custom args
- * <Link to='/b' transition={() => glide({...args})}>
- *   B
- * </Link>
- *
- * // your custom css classname for transitions
- * <Link to='/c' transition='my-css-animation'>
- *   C
- * </Link>
- *
- * @footer
- * \*Refer to [transitions API](/docs/transitions), for more details about
- * transitions.
  *
  */
 const Link = React.forwardRef(({
@@ -86,11 +64,50 @@ Link.displayName = 'TigerLink';
 
 Link.propTypes = {
   /**
-   * Transition/animation to be applied when changing route. String if using
-   * CSS animations. Object or a function returning an object with props to be
-   * passed to [`<Transition />`](https://reactcommunity.org/react-transition-group/transition) or
-   * [`CSSTransition />`](https://reactcommunity.org/react-transition-group/css-transition).
-   * Default value comes from context, defined in [`<Navigation />`](/docs/navigation) component.
+   * Transition to be applied when changing route. It is passed down to
+   * [`<CSSTransition />`](https://reactcommunity.org/react-transition-group/css-transition)
+   *
+   * Default value comes from context, defined in
+   * [`<Navigation />`](/docs/navigation) component.
+   *
+   * If string, it is the `classNames` prop:
+   *
+   * ```javascript
+   * <Link transition='my-animation' {...linkProps}>
+   *    {linkChildren}
+   * </Link>
+   * ```
+   *
+   * Object:
+   *
+   * ```javascript
+   * <Link
+   *    transition={{
+   *      classNames: 'my-animation',
+   *      ...moreProps
+   *    }}
+   *    {...linkProps}
+   * >
+   *    {linkChildren}
+   * </Link>
+   * ```
+   *
+   * A function that return props for CSSTransition:
+   *
+   * ```javascript
+   * <Link
+   *    transition={() => {
+   *      // ...some logic
+   *      return ({
+   *        classNames: 'my-animation',
+   *        ...moreProps
+   *      })
+   *    }}
+   *    {...linkProps}
+   * >
+   *    {linkChildren}
+   * </Link>
+   * ```
    */
   transition: PropTypes.oneOfType([
     PropTypes.string,
@@ -100,12 +117,30 @@ Link.propTypes = {
 
   /**
    * Function fired on link click.
+   *
+   * ```javascript
+   * <Link
+   *    onClick={() => console.log("I execute after link click")}
+   *    {...linkProps}
+   * >
+   *    {linkChildren}
+   * </Link>
+   * ```
    */
   onClick: PropTypes.func,
 
   /**
    * Transition timeout in milliseconds. Used as fallback if not provided on
-   * transition object / function. Used on css transitions.
+   * `transition` prop object / function.
+   *
+   * ```javascript
+   * <Link
+   *    timeout={600} //default value is overwritten if provided in transition prop.
+   *    {...linkProps}
+   * >
+   *    {linkChildren}
+   * </Link>
+   * ```
    */
   timeout: PropTypes.number,
 };
